5d1447760e34396156f6a9acc9ab1535f2c6d74e
kdjun97 <kdjun97@gmail.com>
2023-12-29 11:39:48 +0900

    init project

---
+++ RecordingTodoApp/RecordingTodoApp/RecordingTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordingTodoAppApp.swift
+//  RecordingTodoApp
+//
+//  Created by 김동준 on 12/29/23
+//
+
+import SwiftUI
+
+@main
+struct RecordingTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
