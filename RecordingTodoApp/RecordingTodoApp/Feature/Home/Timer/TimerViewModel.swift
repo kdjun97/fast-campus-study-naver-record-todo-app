be12020ac574aa86033a727d742d88a57ce67300
kdjun97 <kdjun97@gmail.com>
2024-01-13 23:05:47 +0900

    feat/timer-notification: add local notification

--- RecordingTodoApp/RecordingTodoApp/Feature/Home/Timer/TimerViewModel.swift
+++ RecordingTodoApp/RecordingTodoApp/Feature/Home/Timer/TimerViewModel.swift
@@ -6,6 +6,7 @@
  //
 
 import Foundation
+import UIKit
 
 class TimerViewModel: ObservableObject {
     @Published var isDisplaySetTimeView: Bool
@@ -13,19 +14,22 @@ class TimerViewModel: ObservableObject {
     @Published var timer: Timer?
     @Published var timeRemaining: Int
     @Published var isPaused: Bool
+    var notificationService: NotificationService
     
     init(
         isDisplaySetTimeView: Bool = true,
         time: Time = .init(hours: 0, minutes: 0, seconds: 0),
         timer: Timer? = nil,
         timeRemaining: Int = 0,
-        isPaused: Bool = false
+        isPaused: Bool = false,
+        notificationService: NotificationService = .init()
     ) {
         self.isDisplaySetTimeView = isDisplaySetTimeView
         self.time = time
         self.timer = timer
         self.timeRemaining = timeRemaining
         self.isPaused = isPaused
+        self.notificationService = notificationService
     }
 }
 
@@ -55,6 +59,16 @@ extension TimerViewModel {
 private extension TimerViewModel {
     func startTimer() {
         guard timer == nil else { return }
+        
+        var backgroundTaskID: UIBackgroundTaskIdentifier?
+        
+        backgroundTaskID = UIApplication.shared.beginBackgroundTask {
+            if let task = backgroundTaskID {
+                UIApplication.shared.endBackgroundTask(task)
+                backgroundTaskID = .invalid
+            }
+        }
+
         timer = Timer.scheduledTimer(
             withTimeInterval: 1,
             repeats: true
@@ -63,6 +77,12 @@ private extension TimerViewModel {
                 self.timeRemaining -= 1
             } else {
                 self.stopTimer()
+                self.notificationService.sendNotification()
+                
+                if let task = backgroundTaskID {
+                    UIApplication.shared.endBackgroundTask(task)
+                    backgroundTaskID = .invalid
+                }
             }
         }
     }
